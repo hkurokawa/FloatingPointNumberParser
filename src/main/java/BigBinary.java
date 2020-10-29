f55d2d9b808f7c326c2e3b3690e912e006257c30
hkurokawa <hirosh.kurokawa@gmail.com>
2020-10-29 11:44:29 +0900

    Add BigBinary.

---
+++ src/main/java/BigBinary.java
@@ -0,0 +1,152 @@
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class BigBinary implements BigNumber {
+  private final LinkedList<Integer> digits = new LinkedList<>(); // Digits from right to left
+  private boolean negative;
+  private int dp; // Location of decimal point from right, e.g. dp = 4 for 1.0001
+
+  public BigBinary(int n) {
+    if (n < 0) {
+      negative = true;
+      n *= -1;
+    }
+    while (n > 0) {
+      digits.add(n % 2);
+      n /= 2;
+    }
+    normalize();
+  }
+
+  public BigBinary(String s) {
+    if (s == null || s.isEmpty()) {
+      throw new IllegalArgumentException("Cannot parse an empty string");
+    }
+    if (s.startsWith("-")) {
+      negative = true;
+      s = s.substring(1);
+    }
+    if (s.startsWith("+")) {
+      s = s.substring(1);
+    }
+    if (!s.startsWith("0x")) {
+      throw new IllegalArgumentException("The string must start with 0x: " + s);
+    }
+    s = s.substring(2);
+    dp = -1;
+    int exp = 0;
+    for (int i = 0; i < s.length(); i++) {
+      char ch = s.charAt(i);
+      int b = convertHexToInt(ch);
+      if (ch == '.') {
+        if (dp >= 0) {
+          throw new IllegalArgumentException("Unexpected decimal point at "
+              + i + ". There are more than one decimal points: " + s);
+        }
+        dp = i * 4;
+      } else if (b >= 0) {
+        digits.add(b >> 3 & 1);
+        digits.add(b >> 2 & 1);
+        digits.add(b >> 1 & 1);
+        digits.add(b & 1);
+      } else if (ch == 'p' || ch == 'P') {
+        exp = Integer.parseInt(s, i + 1, s.length(), 10);
+        i = s.length();
+      } else {
+        throw new IllegalArgumentException("Unexpected char at " + i + ": " + s);
+      }
+    }
+    if (dp < 0) {
+      dp = digits.size();
+    }
+    Collections.reverse(digits);
+    dp = digits.size() - dp;
+    dp -= exp;
+    normalize();
+  }
+
+  @Override public boolean isNegative() {
+    return negative;
+  }
+
+  @Override public void multiplyByTwo() {
+    dp--;
+    normalize();
+  }
+
+  @Override public void divideByTwo() {
+    dp++;
+    normalize();
+  }
+
+  @Override public boolean isLessThanOne() {
+    return dp == digits.size() - 1 && digits.getLast() == 0;
+  }
+
+  @Override public boolean isEqualToOrGreaterThanTwo() {
+    if (dp != digits.size() - 1) return true; // >= 10
+    return digits.getLast() > 1;
+  }
+
+  @Override public void discardNumberPart() {
+    while (dp < digits.size() - 1) digits.removeLast();
+    digits.set(digits.size() - 1, 0);
+  }
+
+  @Override public boolean isZero() {
+    return digits.size() == 1 && digits.getFirst() == 0;
+  }
+
+  private int convertHexToInt(char ch) {
+    if (ch >= '0' && ch <= '9') {
+      return ch - '0';
+    } else if (ch >= 'a' && ch <= 'f') {
+      return ch - 'a' + 10;
+    } else if (ch >= 'A' && ch <= 'F') {
+      return ch - 'A' + 10;
+    }
+    return -1;
+  }
+
+  private void normalize() {
+    while (dp >= digits.size()) {
+      digits.addLast(0);
+    }
+    while (dp < 0) {
+      digits.addFirst(0);
+      dp++;
+    }
+    while (digits.getFirst() == 0 && dp > 0) {
+      digits.removeFirst();
+      dp--;
+    }
+    while (digits.getLast() == 0 && dp < digits.size() - 1) {
+      digits.removeLast();
+    }
+  }
+
+  @Override public String toString() {
+    StringBuilder sb = new StringBuilder();
+    Iterator<Integer> iter = digits.descendingIterator();
+    for (int i = digits.size() - 1; iter.hasNext() && i >= 0; i--) {
+      sb.append(iter.next());
+      if (i == dp) sb.append('.');
+    }
+    return sb.toString();
+  }
+
+  @Override public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    BigBinary that = (BigBinary) o;
+    return negative == that.negative &&
+        dp == that.dp &&
+        digits.equals(that.digits);
+  }
+
+  @Override public int hashCode() {
+    return Objects.hash(digits, negative, dp);
+  }
+}
---
+++ src/test/java/BigBinaryTest.java
@@ -0,0 +1,108 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BigBinaryTest {
+  @Test
+  public void constructor_One() {
+    assertThat(new BigBinary("0x1")).isEqualTo(new BigBinary(1));
+  }
+
+  @Test
+  public void constructor_FortyTwo() {
+    assertThat(new BigBinary("0x2A")).isEqualTo(new BigBinary(42));
+  }
+
+  @Test
+  public void constructor_Half() {
+    assertThat(new BigBinary("0x.8")).isEqualTo(new BigBinary("0x1.p-1"));
+  }
+
+  @Test
+  public void constructor_QuarterOfQuarter() {
+    assertThat(new BigBinary("0x.1")).isEqualTo(new BigBinary("0x1p-4"));
+  }
+
+  @Test
+  public void constructor_Minus255() {
+    assertThat(new BigBinary("-0xff")).isEqualTo(new BigBinary(-255));
+  }
+
+  @Test
+  public void constructor_Zero() {
+    assertThat(new BigBinary("0x0p1024")).isEqualTo(new BigBinary(0));
+  }
+
+  @Test
+  public void isNegative_Negative() {
+    assertThat(new BigBinary("-0x0").isNegative()).isTrue();
+  }
+
+  @Test
+  public void isNegative_Positive() {
+    assertThat(new BigBinary("0x0").isNegative()).isFalse();
+  }
+
+  @Test
+  public void multiplyByTwo() {
+    var b = new BigBinary("0x0.8");
+    b.multiplyByTwo();
+    assertThat(b).isEqualTo(new BigBinary(1));
+  }
+
+  @Test
+  public void divideByTwo() {
+    var b = new BigBinary("0x10");
+    b.divideByTwo();
+    assertThat(b).isEqualTo(new BigBinary(8));
+  }
+
+  @Test
+  public void isLessThanOne_One() {
+    assertThat(new BigBinary(1).isLessThanOne()).isFalse();
+  }
+
+  @Test
+  public void isLessThanOne_Eight() {
+    assertThat(new BigBinary(8).isLessThanOne()).isFalse();
+  }
+
+  @Test
+  public void isLessThanOne_Half() {
+    assertThat(new BigBinary("0x.8").isLessThanOne()).isTrue();
+  }
+
+  @Test
+  public void isEqualToOrGreaterThanTwo_One() {
+    assertThat(new BigBinary(1).isEqualToOrGreaterThanTwo()).isFalse();
+  }
+
+  @Test
+  public void isEqualToOrGreaterThanTwo_1024() {
+    assertThat(new BigBinary("0x1p10").isEqualToOrGreaterThanTwo()).isTrue();
+  }
+
+  @Test
+  public void isEqualToOrGreaterThanTwo_CloseToTwo() {
+    assertThat(new BigBinary("0x1.ffffffffffffffff").isEqualToOrGreaterThanTwo()).isFalse();
+  }
+
+  @Test
+  public void discardNumberPart_1024AndHalf() {
+    var b = new BigBinary("0x1.002p10");
+    b.discardNumberPart();
+    assertThat(b).isEqualTo(new BigBinary("0x0.8"));
+  }
+
+  @Test
+  public void discardNumberPart_Quarter() {
+    var b = new BigBinary("0x1p-2");
+    b.discardNumberPart();
+    assertThat(b).isEqualTo(new BigBinary("0x0.4"));
+  }
+
+  @Test
+  public void isZero() {
+    assertThat(new BigBinary("0x0p100").isZero()).isTrue();
+  }
+}
